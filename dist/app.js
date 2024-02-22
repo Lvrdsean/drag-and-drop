7ccc5cae914edebf2422b0f5c6757d9cedd35123
Sean Khan <seanny813@optonline.net>
2024-02-22 14:58:31 -0500

    Starting Files

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+class Department {
+  constructor(n) {
+    this.name = n;
+  }
+}
+
+const accounting = new Department("Accounting");
+console.log(accounting);
+//#sourceMappingURL=app.js.map
