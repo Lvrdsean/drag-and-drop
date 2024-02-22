ee036a15812f3c51e76e7dfbf891f2d9294bd0c1
Sean Khan <seanny813@optonline.net>
2024-02-22 15:21:48 -0500

    DOM Element Selection

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,14 @@
-"use strict";
-class Department {
-  constructor(n) {
-    this.name = n;
-  }
-}
-
-const accounting = new Department("Accounting");
-console.log(accounting);
-//#sourceMappingURL=app.js.map
+var ProjectInput = /** @class */ (function () {
+    function ProjectInput() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    ProjectInput.prototype.attach = function () {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    };
+    return ProjectInput;
+}());
+var prjInput = new ProjectInput();
