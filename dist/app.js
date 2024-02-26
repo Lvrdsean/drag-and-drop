535cae91f2be916d7f7311d606479ca5cb98bde1
Sean Khan <seanny813@optonline.net>
2024-02-26 10:20:11 -0500

    Added Inheritance & Generics

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
     var useValue = arguments.length > 2;
     for (var i = 0; i < initializers.length; i++) {
@@ -48,10 +63,21 @@ var Project = /** @class */ (function () {
     }
     return Project;
 }());
-var ProjectState = /** @class */ (function () {
-    function ProjectState() {
+var State = /** @class */ (function () {
+    function State() {
         this.listeners = [];
-        this.projects = [];
+    }
+    State.prototype.addListener = function (listenerFn) {
+        this.listeners.push(listenerFn);
+    };
+    return State;
+}());
+var ProjectState = /** @class */ (function (_super) {
+    __extends(ProjectState, _super);
+    function ProjectState() {
+        var _this = _super.call(this) || this;
+        _this.projects = [];
+        return _this;
     }
     ProjectState.getInstance = function () {
         if (this.instance) {
@@ -59,9 +85,6 @@ var ProjectState = /** @class */ (function () {
         }
         this.instance = new ProjectState();
         return this.instance;
-    };
-    ProjectState.prototype.addListener = function (listenerFn) {
-        this.listeners.push(listenerFn);
     };
     ProjectState.prototype.addProject = function (title, description, numOfPeople) {
         var newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);
@@ -72,7 +95,7 @@ var ProjectState = /** @class */ (function () {
         }
     };
     return ProjectState;
-}());
+}(State));
 var projectState = ProjectState.getInstance();
 function validate(validatableInput) {
     var isValid = true;
@@ -114,17 +137,36 @@ function autobind(_, _2, descriptor) {
     };
     return adjDescriptor;
 }
-// ProjectList Class
-var ProjectList = /** @class */ (function () {
-    function ProjectList(type) {
-        var _this = this;
-        this.type = type;
-        this.templateElement = document.getElementById("project-list");
-        this.hostElement = document.getElementById("app");
-        this.assignedProjects = [];
+// Component Base Class
+var Component = /** @class */ (function () {
+    function Component(templateId, hostElementId, insertAtStart, newElementId) {
+        this.templateElement = document.getElementById(templateId);
+        this.hostElement = document.getElementById(hostElementId);
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
-        this.element.id = "".concat(this.type, "-projects");
+        if (newElementId) {
+            this.element.id = newElementId;
+        }
+        this.attach(insertAtStart);
+    }
+    Component.prototype.attach = function (insertAtBeginning) {
+        this.hostElement.insertAdjacentElement(insertAtBeginning ? "afterbegin" : "beforeend", this.element);
+    };
+    return Component;
+}());
+// ProjectList Class
+var ProjectList = /** @class */ (function (_super) {
+    __extends(ProjectList, _super);
+    function ProjectList(type) {
+        var _this = _super.call(this, "project-list", "app", false, "".concat(type, "-projects")) || this;
+        _this.type = type;
+        _this.assignedProjects = [];
+        _this.configure();
+        _this.renderContent();
+        return _this;
+    }
+    ProjectList.prototype.configure = function () {
+        var _this = this;
         projectState.addListener(function (projects) {
             var relevantProjects = projects.filter(function (prj) {
                 if (_this.type === "active") {
@@ -135,9 +177,13 @@ var ProjectList = /** @class */ (function () {
             _this.assignedProjects = relevantProjects;
             _this.renderProjects();
         });
-        this.attach();
-        this.renderContent();
-    }
+    };
+    ProjectList.prototype.renderContent = function () {
+        var listId = "".concat(this.type, "-projects-list");
+        this.element.querySelector("ul").id = listId;
+        this.element.querySelector("h2").textContent =
+            this.type.toUpperCase() + " PROJECTS";
+    };
     ProjectList.prototype.renderProjects = function () {
         var listEl = document.getElementById("".concat(this.type, "-projects-list"));
         listEl.innerHTML = "";
@@ -148,36 +194,29 @@ var ProjectList = /** @class */ (function () {
             listEl.appendChild(listItem);
         }
     };
-    ProjectList.prototype.renderContent = function () {
-        var listId = "".concat(this.type, "-projects-list");
-        this.element.querySelector("ul").id = listId;
-        this.element.querySelector("h2").textContent =
-            this.type.toUpperCase() + " PROJECTS";
-    };
-    ProjectList.prototype.attach = function () {
-        this.hostElement.insertAdjacentElement("beforeend", this.element);
-    };
     return ProjectList;
-}());
+}(Component));
 // ProjectInput Class
 var ProjectInput = function () {
     var _a;
+    var _classSuper = Component;
     var _instanceExtraInitializers = [];
     var _submitHandler_decorators;
-    return _a = /** @class */ (function () {
+    return _a = /** @class */ (function (_super) {
+            __extends(ProjectInput, _super);
             function ProjectInput() {
-                this.templateElement = (__runInitializers(this, _instanceExtraInitializers), void 0);
-                this.templateElement = document.getElementById("project-input");
-                this.hostElement = document.getElementById("app");
-                var importedNode = document.importNode(this.templateElement.content, true);
-                this.element = importedNode.firstElementChild;
-                this.element.id = "user-input";
-                this.titleInputElement = this.element.querySelector("#title");
-                this.descriptionInputElement = this.element.querySelector("#description");
-                this.peopleInputElement = this.element.querySelector("#people");
-                this.configure();
-                this.attach();
+                var _this = _super.call(this, "project-input", "app", true, "user-input") || this;
+                _this.titleInputElement = (__runInitializers(_this, _instanceExtraInitializers), void 0);
+                _this.titleInputElement = _this.element.querySelector("#title");
+                _this.descriptionInputElement = _this.element.querySelector("#description");
+                _this.peopleInputElement = _this.element.querySelector("#people");
+                _this.configure();
+                return _this;
             }
+            ProjectInput.prototype.configure = function () {
+                this.element.addEventListener("submit", this.submitHandler);
+            };
+            ProjectInput.prototype.renderContent = function () { };
             ProjectInput.prototype.gatherUserInput = function () {
                 var enteredTitle = this.titleInputElement.value;
                 var enteredDescription = this.descriptionInputElement.value;
@@ -221,16 +260,11 @@ var ProjectInput = function () {
                     this.clearInputs();
                 }
             };
-            ProjectInput.prototype.configure = function () {
-                this.element.addEventListener("submit", this.submitHandler);
-            };
-            ProjectInput.prototype.attach = function () {
-                this.hostElement.insertAdjacentElement("afterbegin", this.element);
-            };
             return ProjectInput;
-        }()),
+        }(_classSuper)),
         (function () {
-            var _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+            var _b;
+            var _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create((_b = _classSuper[Symbol.metadata]) !== null && _b !== void 0 ? _b : null) : void 0;
             _submitHandler_decorators = [autobind];
             __esDecorate(_a, null, _submitHandler_decorators, { kind: "method", name: "submitHandler", static: false, private: false, access: { has: function (obj) { return "submitHandler" in obj; }, get: function (obj) { return obj.submitHandler; } }, metadata: _metadata }, null, _instanceExtraInitializers);
             if (_metadata) Object.defineProperty(_a, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
